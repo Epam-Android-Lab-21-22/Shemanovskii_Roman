937677069bd6e3798fda05ecce2b388676e74773
Roman <cborzyy@gmail.com>
2022-01-17 20:36:10 +0300

    Hotfix

--- app/src/main/java/com/beleavemebe/solevarnya/presentation/ui/fragments/timetable/TimetableFragment.kt
+++ app/src/main/java/com/beleavemebe/solevarnya/presentation/ui/fragments/timetable/TimetableFragment.kt
@@ -9,8 +9,6 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.beleavemebe.solevarnya.R
 import com.beleavemebe.solevarnya.core.domain.Lesson
 import com.beleavemebe.solevarnya.databinding.FragmentTimetableBinding
-import com.beleavemebe.solevarnya.presentation.repository.InMemoryLessonDataSource
-import com.beleavemebe.solevarnya.presentation.ui.fragments.BaseListView
 import com.beleavemebe.solevarnya.presentation.ui.fragments.Constants.ITEM_MARGIN
 
 class TimetableFragment :
@@ -20,24 +18,16 @@ class TimetableFragment :
     private val binding by viewBinding(FragmentTimetableBinding::bind)
     private val presenter = TimetablePresenter(this)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvTimetable.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTimetable.addItemDecoration(TimetableDecoration(ITEM_MARGIN))
         presenter.onRecyclerReady()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.rvTimetable.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            addItemDecoration(TimetableDecoration(ITEM_MARGIN))
-        }
-    }
-
     override fun setContent(content: List<Lesson>) {
-        binding.rvTimetable.apply {
-            val timetableAdapter = TimetableAdapter(content, ::loadMore)
-            adapter = timetableAdapter
-        }
+        val timetableAdapter = TimetableAdapter(content, ::loadMore)
+        binding.rvTimetable.adapter = timetableAdapter
     }
 
     override fun onDestroy() {
