47e97f3a3935a4d1fdc9b2b8d0cf9c3b079a5741
Roman <cborzyy@gmail.com>
2022-01-30 00:47:09 +0300

    Fix ExternalStorageNoteRepository.kt

--- app/src/main/java/com/beleavemebe/solevarnya/data/ExternalStorageNoteRepository.kt
+++ app/src/main/java/com/beleavemebe/solevarnya/data/ExternalStorageNoteRepository.kt
@@ -3,17 +3,8 @@ package com.beleavemebe.solevarnya.data
 import android.content.Context
 import java.io.File
 
-private const val FILE_NAME_NOTES = "notes_external.json"
-
 class ExternalStorageNoteRepository(context: Context) : JsonFileNoteRepository() {
 
-    private val externalStorageDir = context.getExternalFilesDir(null)
+    override val jsonFile = TODO()
 
-    override val jsonFile = externalStorageDir?.run {
-        File("$path/$FILE_NAME_NOTES")
-    }
-
-    init {
-        jsonFile?.createNewFile()
-    }
 }
