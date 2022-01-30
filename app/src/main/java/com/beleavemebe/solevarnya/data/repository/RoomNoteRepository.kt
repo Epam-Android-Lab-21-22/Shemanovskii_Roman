e0dd4c5eb37fac12b0fec2356fd3cd8cad56b92d
Roman <cborzyy@gmail.com>
2022-01-31 00:39:39 +0300

    Get rid of `NoteMapper` in favor of extension function

--- app/src/main/java/com/beleavemebe/solevarnya/data/repository/RoomNoteRepository.kt
+++ app/src/main/java/com/beleavemebe/solevarnya/data/repository/RoomNoteRepository.kt
@@ -3,10 +3,10 @@ package com.beleavemebe.solevarnya.data.repository
 import android.content.Context
 import androidx.room.Room
 import com.beleavemebe.solevarnya.data.db.AppDatabase
+import com.beleavemebe.solevarnya.data.model.NoteDto
 import com.beleavemebe.solevarnya.domain.model.Note
 import com.beleavemebe.solevarnya.domain.repository.NoteRepository
-import com.beleavemebe.solevarnya.mapper.NoteMapper
-import java.io.IOException
+import java.util.*
 
 class RoomNoteRepository(context: Context) : NoteRepository {
     private val database = Room.databaseBuilder(
@@ -18,14 +18,24 @@ class RoomNoteRepository(context: Context) : NoteRepository {
     private val dao = database.noteDao()
 
     override suspend fun fetchAll(): List<Note> =
-        dao.fetchAll()
-            .map {
-                NoteMapper.noteDtoToNote(it)
-            }
+        dao.fetchAll().map(NoteDto::toNote)
 
     override suspend fun add(note: Note) =
-        dao.add(
-            NoteMapper.noteToNoteDto(note)
-        )
+        dao.add(note.toDto())
 
+}
+
+private fun NoteDto.toNote(): Note {
+    return Note(
+        text = text,
+        date = Date(date)
+    )
+}
+
+private fun Note.toDto(): NoteDto {
+    return NoteDto(
+        id = 0,
+        text = text,
+        date = date.time
+    )
 }
